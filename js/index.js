5d8006cf6c9a9d3382f3c21f5e68949c2169d1fb
Daiana Baldovi <daianabaldovi@gmail.com>
2022-11-17 09:31:25 -0300

    index.js

---
+++ js/index.js
@@ -0,0 +1,19 @@
+console.log ("Código del archivo externo");
+
+var nombre;
+
+nombre = "Daiana";
+
+console.log ("Mi nombre es: " + nombre);
+
+var apellido;
+
+apellido = "Baldovi";
+
+console.log ("Mi apellido es: " + apellido);
+
+var edad;
+
+edad = 32 
+
+console.log ("Mi edad es: " + 32);
